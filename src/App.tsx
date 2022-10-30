1d87f2df2e6a8d2b7024730c8b21c8ab07d06212
Eleri Ojavere <eleri.ojavere@gmail.com>
2022-10-30 18:18:01 +0200

    Styling using scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import Map from "./js/components/Map";
 import "./scss/entry.scss";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="max-width-wrapper">
+      <h1>Venues</h1>
+      <Map />
+    </div>
+  );
 }
 
 export default App;
